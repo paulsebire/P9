3ff192e666ec3370826c57d287412baeb8fd8f24
Paul <psebire37@gmail.com>
2020-06-15 16:54:27 +0200

    first commit

---
+++ src/myerp-business/src/test/java/com/dummy/myerp/business/impl/manager/ComptabiliteManagerImplTest.java
@@ -0,0 +1,104 @@
+package com.dummy.myerp.business.impl.manager;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import com.dummy.myerp.model.bean.comptabilite.*;
+import com.dummy.myerp.technical.exception.FunctionalException;
+
+
+public class ComptabiliteManagerImplTest {
+
+    private ComptabiliteManagerImpl manager = new ComptabiliteManagerImpl();
+
+    private EcritureComptable vEcritureComptable;
+
+
+    @Before
+    public void initCompatibiliteManagerImpl(){
+        vEcritureComptable = new EcritureComptable();
+        vEcritureComptable.setJournal(new JournalComptable("AC", "Achat"));
+        vEcritureComptable.setDate(new Date());
+        vEcritureComptable.setLibelle("Libelle");
+        vEcritureComptable.getListLigneEcriture().add(new LigneEcritureComptable(new CompteComptable(1),
+                null, new BigDecimal(123),
+                null));
+        vEcritureComptable.getListLigneEcriture().add(new LigneEcritureComptable(new CompteComptable(2),
+                null, null,
+                new BigDecimal(123)));
+    }
+    @After
+    public void ResetvEcritureComptable(){
+        vEcritureComptable=new EcritureComptable();
+    }
+
+    @Test
+    public void checkEcritureComptableUnit() throws Exception {
+        manager.checkEcritureComptableUnit(vEcritureComptable);
+    }
+
+    @Test
+    public void checkInitCompatibiliteManagerImplViolations() throws Exception {
+        manager.checkEcritureComptableViolations(vEcritureComptable);
+    }
+
+    @Test(expected = FunctionalException.class)
+    public void checkEcritureComptableUnitViolation() throws Exception {
+        EcritureComptable vEcritureComptable= new EcritureComptable();
+        manager.checkEcritureComptableViolations(vEcritureComptable);
+    }
+
+    @Test(expected = FunctionalException.class)
+    public void checkEcritureComptableUnitRG2() throws Exception {
+        EcritureComptable vEcritureComptable;
+        vEcritureComptable = new EcritureComptable();
+        vEcritureComptable.setJournal(new JournalComptable("AC", "Achat"));
+        vEcritureComptable.setDate(new Date());
+        vEcritureComptable.setLibelle("Libelle");
+        vEcritureComptable.getListLigneEcriture().add(new LigneEcritureComptable(new CompteComptable(1),
+                                                                                 null, new BigDecimal(123),
+                                                                                 null));
+        vEcritureComptable.getListLigneEcriture().add(new LigneEcritureComptable(new CompteComptable(2),
+                                                                                 null, null,
+                                                                                 new BigDecimal(1234)));
+        manager.checkEcritureComptableEquilibree(vEcritureComptable);
+    }
+
+    @Test(expected = FunctionalException.class)
+    public void checkEcritureComptableUnitRG3() throws Exception {
+        EcritureComptable vEcritureComptable;
+        vEcritureComptable = new EcritureComptable();
+        vEcritureComptable.setJournal(new JournalComptable("AC", "Achat"));
+        vEcritureComptable.setDate(new Date());
+        vEcritureComptable.setLibelle("Libelle");
+        vEcritureComptable.getListLigneEcriture().add(new LigneEcritureComptable(new CompteComptable(1),
+                                                                                 null, new BigDecimal(123),
+                                                                                 null));
+        vEcritureComptable.getListLigneEcriture().add(new LigneEcritureComptable(new CompteComptable(1),
+                                                                                 null, new BigDecimal(123),
+                                                                                 null));
+        manager.checkNumberOflineInEcriture(vEcritureComptable);
+    }
+
+
+
+    @Test(expected = FunctionalException.class)
+    public void checkEcritureComptableUnitRG7() throws Exception {
+        EcritureComptable vEcritureComptable;
+        vEcritureComptable = new EcritureComptable();
+        vEcritureComptable.setJournal(new JournalComptable("AC", "Achat"));
+        vEcritureComptable.setDate(new Date());
+        vEcritureComptable.setLibelle("Libelle");
+        vEcritureComptable.getListLigneEcriture().add(new LigneEcritureComptable(new CompteComptable(1),
+                null, new BigDecimal(123.562),
+                null));
+        vEcritureComptable.getListLigneEcriture().add(new LigneEcritureComptable(new CompteComptable(1),
+                null,null,
+                new BigDecimal(123)));
+        manager.checkEcritureComptableViolations(vEcritureComptable);
+    }
+}
---
+++ src/myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/EcritureComptableTest.java
@@ -0,0 +1,83 @@
+package com.dummy.myerp.model.bean.comptabilite;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import org.apache.commons.lang3.ObjectUtils;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class EcritureComptableTest {
+
+    private LigneEcritureComptable createLigne(Integer pCompteComptableNumero, String pDebit, String pCredit) {
+        BigDecimal vDebit = pDebit == null ? null : new BigDecimal(pDebit);
+        BigDecimal vCredit = pCredit == null ? null : new BigDecimal(pCredit);
+        String vLibelle = ObjectUtils.defaultIfNull(vDebit, BigDecimal.ZERO)
+                                     .subtract(ObjectUtils.defaultIfNull(vCredit, BigDecimal.ZERO)).toPlainString();
+        LigneEcritureComptable vRetour = new LigneEcritureComptable(new CompteComptable(pCompteComptableNumero),
+                                                                    vLibelle,
+                                                                    vDebit, vCredit);
+        return vRetour;
+    }
+
+    private EcritureComptable vEcritureComptable;
+
+
+    @Before
+    public void initCompatibiliteManagerImpl(){
+        vEcritureComptable = new EcritureComptable();
+        vEcritureComptable.setJournal(new JournalComptable("AC", "Achat"));
+        vEcritureComptable.setDate(new Date());
+        vEcritureComptable.setLibelle("Libelle");
+        vEcritureComptable.getListLigneEcriture().add(new LigneEcritureComptable(new CompteComptable(1),
+                null, new BigDecimal(123),
+                null));
+        vEcritureComptable.getListLigneEcriture().add(new LigneEcritureComptable(new CompteComptable(2),
+                null, null,
+                new BigDecimal(123)));
+    }
+
+    @Test
+    public void isEquilibree() {
+        EcritureComptable vEcriture;
+        vEcriture = new EcritureComptable();
+
+        vEcriture.setLibelle("Equilibrée");
+        vEcriture.getListLigneEcriture().add(this.createLigne(1, "200.50", null));
+        vEcriture.getListLigneEcriture().add(this.createLigne(1, "100.50", "33"));
+        vEcriture.getListLigneEcriture().add(this.createLigne(2, null, "301"));
+        vEcriture.getListLigneEcriture().add(this.createLigne(2, "40", "7"));
+        Assert.assertTrue(vEcriture.toString(), vEcriture.isEquilibree());
+    }
+    @Test
+    public void isNotEquilibree() {
+        EcritureComptable vEcriture;
+        vEcriture = new EcritureComptable();
+        vEcriture.getListLigneEcriture().clear();
+        vEcriture.setLibelle("Non équilibrée");
+        vEcriture.getListLigneEcriture().add(this.createLigne(1, "10", null));
+        vEcriture.getListLigneEcriture().add(this.createLigne(1, "20", "1"));
+        vEcriture.getListLigneEcriture().add(this.createLigne(2, null, "30"));
+        vEcriture.getListLigneEcriture().add(this.createLigne(2, "1", "2"));
+        Assert.assertFalse(vEcriture.toString(), vEcriture.isEquilibree());
+    }
+
+    @Test
+    public void checkMontantLigneEcritureSignes(){
+        vEcritureComptable.getListLigneEcriture().clear();
+        vEcritureComptable.getListLigneEcriture().add(new LigneEcritureComptable(new CompteComptable(1),
+                null, new BigDecimal(-543.21),
+                new BigDecimal(-123.45)));
+
+        Assert.assertEquals(vEcritureComptable.getTotalDebit(),BigDecimal.valueOf(-543.21));
+        Assert.assertNotEquals(vEcritureComptable.getTotalDebit(),BigDecimal.valueOf(543.21));
+
+        Assert.assertEquals(vEcritureComptable.getTotalCredit(),BigDecimal.valueOf(-123.45));
+        Assert.assertNotEquals(vEcritureComptable.getTotalCredit(),BigDecimal.valueOf(123.45));
+
+
+    }
+
+}
